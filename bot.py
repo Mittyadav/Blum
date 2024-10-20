f4b91d74b25859310e7118a91219d6292e29536d
Mittyadav <146659530+Mittyadav@users.noreply.github.com>
2024-10-20 16:26:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -448,9 +448,10 @@ async def get_data(data_file, proxy_file):
 async def main():
     banner = f"""
 {magenta} _____ __    _____ _____  {white}BLUM AUTO TASK AND PLAY GAME BOT {green}BLUM
-{magenta}| __  |  |  |  |  |     | {green}Author : {white}D4rkCipherX
-{magenta}| __ -|  |__|  |  | | | | {white}YouTube : {green}https://youtube.com/@d4rkcipherx
-{magenta}|_____|_____|_____|_|_|_| {green}Note : {white}MUST SUBSCRIBE MY YOUTUBE CHANNEL
+{magenta}| __  |  |  |  |  |     | {green}Author : {white} @iamak_roy
+{magenta}| __ -|  |__|  |  | | | | {white}YouTube : {green} @scripthub00
+{magenta}|_____|_____|_____|_|_|_| {green}Note : {white}MUST join telegram CHANNEL
+        made with ❣️by dark life
         """
     arg = argparse.ArgumentParser()
     arg.add_argument(
